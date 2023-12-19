eb003034e2d4d0ea9cdc80d01f5d618048652a44
bassem <basem12683@gmail.com>
2023-12-19 04:50:10 +0300

    text='tech stack'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
+import Skills from "./components/Skills"
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Hero/>
+      <Skills/>
     </Fragment>
   );
 };
