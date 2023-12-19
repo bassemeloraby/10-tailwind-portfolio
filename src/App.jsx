c488a17b63916c509b770be1a2623d4c90271acf
bassem <basem12683@gmail.com>
2023-12-19 04:57:21 +0300

    About

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills"
+import About from "./components/About";
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Hero/>
       <Skills/>
+      <About/>
     </Fragment>
   );
 };
