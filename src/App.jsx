c9a1111f269d871f3df7e06f2244327796739ac7
bassem <basem12683@gmail.com>
2023-12-19 04:07:36 +0300

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <Fragment>
+      <Navbar />
+      <Hero/>
+    </Fragment>
+  );
+};
 
-export default App
+export default App;
